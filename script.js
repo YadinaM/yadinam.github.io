10a359a3f15af391315a93de5d47c3f1002b7e65
Sarah-Lisa Mero <sarahlisamero@gmail.com>
2024-05-13 19:38:27 +0200

    feat: faq dropdown

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.querySelector('.hamburger').addEventListener('click', function() {
     document.querySelector('.klik').classList.toggle('active');
 });
+function toggleAnswer(id) {
+    var answer = document.getElementById('answer' + id);
+    answer.style.display = (answer.style.display === 'block') ? 'none' : 'block';
+}
